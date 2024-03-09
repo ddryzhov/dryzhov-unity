2652545d1745240ca89583d75f868561db0a98bb
danX <danildryzov@gmail.com>
2024-03-09 22:13:05 +0100

    modified Dis script

--- Assets/Destructible.cs
+++ Assets/Destructible.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Destructible : MonoBehaviour
 {
     public GameObject destroyedVersion; // Assign a prefab of the destroyed barrel
+    private bool hasBeenDestroyed = false;
 
     public void Destroy()
     {
         // Instantiate the destroyed version of the barrel
-        if (destroyedVersion != null)
+        if (!hasBeenDestroyed && destroyedVersion != null)
         {
             GameObject destroyedInstance = Instantiate(destroyedVersion, transform.position, transform.rotation);
             destroyedInstance.transform.localScale = Vector3.one * 13;
+            hasBeenDestroyed = true;
         }
 
         // Disable the original barrel
